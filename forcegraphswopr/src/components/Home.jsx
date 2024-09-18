4a83bbd41638c904401996dfd31ad9f030c884b0
rlores <raquel.lores@edison.es>
2024-09-18 22:00:04 +0200

    create_forceGraph_component

---
+++ forcegraphswopr/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import ForceGraph from "./ForceGraph.jsx";
+export function Home() {
+  return (
+    <div>
+    <h1>Nodes Graph</h1>
+    <ForceGraph/>
+    </div>
+  );
+}
+
+export default Home;
