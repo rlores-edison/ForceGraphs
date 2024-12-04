be93df29128ee409d3bdf004e132d4f655e7f850
rlores-edison <raquel.lores@edison.es>
2024-12-04 17:03:35 +0100

    feat(vitest):create unit tests

--- forcegraphswopr/vite.config.js
+++ forcegraphswopr/vite.config.js
@@ -1,19 +1,23 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-force-graph', 'three'],
-    },
-    build: {
-    target: 'esnext' // Ensures modern JavaScript is supported
+    include: ["react-force-graph", "three"],
+  },
+  build: {
+    target: "esnext", // Ensures modern JavaScript is supported
   },
   resolve: {
     alias: {
-      '@': '/src/components' // Use this to simplify imports
-    }
-  }
+      "@": "/src/components", // Use this to simplify imports
+    },
+  },
+  test: {
+    environment: "happy-dom",
+    setupFiles: ['./src/tests/NodeCard.test.jsx'],
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'] 
+  },
 });
